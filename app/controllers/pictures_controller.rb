156362e0238bc20bd13fc659fe682ed745f3ef99
minori chikano <bcloudabc@gmail.com>
2023-06-06 19:33:19 +0900

    コントローラののストロングパラメータ実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,5 +3,17 @@ class PicturesController < ApplicationController
   end
 
   def new
+    @picture = Picture.new
+  end
+
+  def create
+    Picture.create(picture_params)
+    redirect_to new_picture_path
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:content)
   end
 end
