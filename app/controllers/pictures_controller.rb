60daaa27fb0726139f14aaf3051d36f190ddbb0d
minori chikano <bcloudabc@gmail.com>
2023-06-06 23:36:21 +0900

    エラー表示

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,8 +8,12 @@ class PicturesController < ApplicationController
   end
 
   def create
-    Picture.create(picture_params)
-    redirect_to new_picture_path
+    @picture = Picture.create(picture_params)
+    if @picture.save
+    redirect_to pictures_path, notice: "作成完了"
+    else
+      render :new
+    end
   end
 
   def show
