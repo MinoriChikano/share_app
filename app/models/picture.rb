dbe900ef37fdd2f2f6fc8c2831ec74697eef866b
minori chikano <bcloudabc@gmail.com>
2023-06-07 17:09:34 +0900

    imageカラムにImageUploader紐付け

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,4 +3,5 @@ class Picture < ApplicationRecord
     has_many :favorites, dependent: :destroy
     has_many :favorite_users, through: :favorites, source: :user
     validates :content, presence: true
+    mount_uploader :image, ImageUploader
 end
