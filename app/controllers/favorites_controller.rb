69c3370d7b92a3ec3312ef5b5265fa8254ae9d73
minori chikano <bcloudabc@gmail.com>
2023-06-07 16:44:00 +0900

    お気に入り機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,11 @@
 class FavoritesController < ApplicationController
+def create
+    favorite = current_user.favorites.create(blog_id: params[:blog_id])
+    redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんの投稿をお気に入り登録しました"
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんの投稿をお気に入り解除しました"
+  end
 end
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,6 +19,7 @@ class PicturesController < ApplicationController
   end
 
   def show
+    @favorite = current_user.favorites.find_by(picture_id: @picture.id)
   end
 
   def edit
