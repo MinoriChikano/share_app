77a4746617e5de98c5c631f2ba6c72986f9147e8
minori chikano <bcloudabc@gmail.com>
2023-06-08 13:16:47 +0900

    プロフィール編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,21 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @pictures = @user.pictures
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "編集しました！"
+      redirect_to user_path
+    else
+      render :edit
+    end
   end
 
   private
