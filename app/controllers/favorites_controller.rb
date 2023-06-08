f2ef5f63041d90235f03b4d8a13c6cdc88d36009
minori chikano <bcloudabc@gmail.com>
2023-06-08 23:21:06 +0900

    お気に入り一覧実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,12 @@
 class FavoritesController < ApplicationController
+
+  def index
+    @favorites = current_user.favorites
+  end
+  
   def create
-      favorite = current_user.favorites.create(picture_id: params[:picture_id])
-      redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんの投稿をお気に入り登録しました"
+    favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんの投稿をお気に入り登録しました"
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  get 'sessions/new'
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:index, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :pictures do
